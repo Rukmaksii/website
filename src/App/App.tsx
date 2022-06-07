69a7417eb21c0115379ac5f717e089228df51e27
EmmanuelVln <emmanuel.vouillon@epita.fr>
2022-06-07 09:10:17 +0200

    AboutUs: updates description for Emmanuel

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -12,25 +12,25 @@ const data: Person[] = [
 	{
 		Name: "Romain Le Miere",
 		Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim",
-		Role: "Chef de projet",
+		Role: "Team Manager",
 		ImagePath: "https://photos.cri.epita.fr/romain.le-miere"
 	},
 	{
 		Name: "Alban Naulin",
 		Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim",
-		Role: "",
+		Role: "AI ",
 		ImagePath: "https://photos.cri.epita.fr/alban.naulin"
 	},
 	{
 		Name: "Lucas Tilly",
 		Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim",
-		Role: "Directeur 3D",
+		Role: "3D Manager",
 		ImagePath: "https://photos.cri.epita.fr/lucas.tilly"
 	},
 	{
 		Name: "Emmanuel Vouillon",
-		Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim",
-		Role: "",
+		Description: "The main designer of the project. Master of the menus but alos the man behind the HUD, the objectives, some items and many other core features of the game. Versitality is key!",
+		Role: "Project designer",
 		ImagePath: "https://photos.cri.epita.fr/emmanuel.vouillon"
 	}
 ]
